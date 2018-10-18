8feaeb74a622f09ed5e6dc67c3caeaa517b94e35
JohnDS4 <DisandoloJJ@cardiff.ac.uk>
2018-10-18 14:07:55 +0100

    Created people that appear in games, along with their room and conversations

---
+++ people.py
@@ -0,0 +1,91 @@
+from map import rooms
+
+people_king = {
+    "id": "king",
+
+    "name": "King",
+
+    "description":
+    """This is the king.""",
+
+    "conversation": [],
+
+    "items": [],
+
+    "room": rooms["Throne Room"]
+}
+
+people_viceroy = {
+    "id": "viceroy",
+
+    "name": "Viceroy",
+
+    "description":
+    """This is the viceroy.""",
+
+    "conversation": [],
+
+    "items": [],
+
+    "room": rooms["Throne Room"]
+}
+
+people_lady = {
+    "id": "lady",
+
+    "name": "Lady",
+
+    "description":
+    """This is the lady.""",
+
+    "conversation": [],
+
+    "items": [],
+
+    "room": rooms["Courtyard"]
+}
+
+people_wizard = {
+    "id": "wizard",
+
+    "name": "Wizard",
+
+    "description":
+    """This is the wizard.""",
+
+    "conversation": [],
+
+    "items": [],
+
+    "room": rooms["Great Hall"]
+}
+
+people_soldier1 = {
+    "id": "soldier1",
+
+    "name": "Soldier 1",
+
+    "description":
+    """This is soldier 1.""",
+
+    "conversation": [],
+
+    "items": [],
+
+    "room": rooms["Battlements"]
+}
+
+people_soldier2 = {
+    "id": "soldier2",
+
+    "name": "Soldier 2",
+
+    "description":
+    """This is soldier 2.""",
+
+    "conversation": [],
+
+    "items": [],
+
+    "room": rooms["Battlements"]
+}
