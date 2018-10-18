cb7531136f4a8338e194230d12a01f1aa1f1df34
John Disandolo <c1810772@gamma.cs.cf.ac.uk>
2018-10-18 13:36:16 +0100

    Basis of map layout

---
+++ map.py
@@ -0,0 +1,89 @@
+from items import *
+
+room_courtyard = {
+    "name": "Courtyard",
+
+    "description":
+    """This is the courtyard. TODO ---""",
+
+    "exits": {},
+
+    "items": [],
+
+    "people": []
+}
+
+room_greathall = {
+    "name": "Great Hall",
+
+    "description":
+    """This is the great hall. TODO ---""",
+
+    "exits": {},
+
+    "items": [],
+
+    "people": []
+}
+
+room_throne = {
+    "name": "Throne Room",
+
+    "description":
+    """This is the throne room. TODO ---""",
+
+    "exits": {},
+
+    "items": [],
+
+    "people": []
+}
+
+room_battlements = {
+    "name": "Battlements",
+
+    "description":
+    """These are the battlements. TODO ---""",
+
+    "exits": {},
+
+    "items": [],
+
+    "people": []
+}
+
+room_anti = {
+    "name": "Anti Room",
+
+    "description":
+    """This is the anti room. TODO ---""",
+
+    "exits": {},
+
+    "items": [],
+
+    "people": []
+}
+
+room_jail = {
+    "name": "Jail",
+
+    "description":
+    """This is the jail. TODO ---""",
+
+    "exits": {},
+
+    "items": [],
+
+    "people": []
+}
+
+
+rooms = {
+    "Courtyard": room_courtyard,
+    "Great Hall": room_greathall,
+    "Throne Room": room_throne,
+    "Battlements": room_battlements,
+    "Anti Room": room_anti,
+    "Jail": room_jail
+}
